2f4497265ea3d51e6622f14fc33f0f48dd5f6aa1
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2019-10-13 15:15:43 -0500

    Create translation.R

---
+++ translation.R
@@ -0,0 +1,32 @@
+install.packages("devtools")
+
+##BiocManager::install("genbankr")
+
+install.packages("backports")
+library(devtools)
+install_git("https://git.bioconductor.org/packages/genbankr")
+library(genbankr)
+
+##source('https://bioconductor.org/biocLite.R')
+##BiocManager::install("BiocUpgrade")
+##BiocManager::install(c("GenomicFeatures", "AnnotationDbi"))
+library("Biostrings")
+library(bio3d)
+#-----------------------------read protein translations ----------------------------------#
+gb = readGenBank("../Dropbox/project_Public_Health_Genomic/input/sequence.gbk")
+tr <- transcripts(gb)
+proteinlist<-tr$gene_id
+l<- length(proteinlist)
+translations<- matrix(NA,ncol = 2,nrow = l)
+
+for(i in 1:l)
+{
+      seq<- as.character(proteins$translation[i,2])
+      translations[i,1]<- paste0(">",proteinlist[i])
+      translations[i,2]<- seq
+  print(i)
+}
+colnames(translations)<- c("Gene_id","translation")
+write.table(x=translations,file="./trans.txt",quote =FALSE,sep="\n",eol = "\n", row.names = FALSE, col.names = FALSE)
+
+
