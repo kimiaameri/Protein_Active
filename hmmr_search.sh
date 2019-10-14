fba7fa71d21e657bce6c32f05b8780a32c8cf440
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2019-10-14 11:27:09 -0500

    Update hmmr_search.sh

--- hmmr_search.sh
+++ hmmr_search.sh
@@ -1,1 +1,8 @@
+#!/bin/sh
+#SBATCH --time=100:00:00   # Run time in hh:mm:ss
+#SBATCH --mem-per-cpu=64gb     # Maximum memory required per CPU (in megabytes)
+#SBATCH --job-name=hmmrsearch
+#SBATCH --error=hmmrsearch.%J.err
+#SBATCH --output=hmmrsearch.%J.out
+cd $WORK/independent2
  ../hmmer/hmmer-3.2.1/src/hmmsearch Pfam-A.hmm trans.txt > ./hmmerfile/s.Areuse.txt
