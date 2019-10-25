05e5afc5f614ffd41c45b7986a5850d672449171
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2019-10-25 18:21:27 -0500

    Update mappinghmmerhit.R

--- mappinghmmerhit.R
+++ mappinghmmerhit.R
@@ -1,15 +1,14 @@
 #---------------------------------------------------------------------------------------------------#
 #                 Please Change the Setwd to the location you download the folder                   #
 #---------------------------------------------------------------------------------------------------#
-setwd("../Documents/")
 source("geneDomain.R")
 source("MutationPosition.R")
 #-----------------------------------------------------------------------#
 #                             read hmmrfiles                             #
 #-----------------------------------------------------------------------#
-hmmrHit <- as.matrix(read.table("../Documents/sort.hit.S.areus.csv",header=F,sep=" ",stringsAsFactors = F))
+hmmrHit <- as.matrix(read.table("/hmmrfile/sort.hit.S.areus.csv",header=F,sep=" ",stringsAsFactors = F))
 length.hmmrHit= nrow(hmmrHit)
-intersections<- list.files("../Documents/intersection/")
+intersections<- list.files("/intersection/")
 Domain.length<- as.numeric(hmmrHit[,4]) - as.numeric(hmmrHit[,3])
 hmmrHit<-cbind(hmmrHit,Domain.length)
 colnames(hmmrHit)<- c("Gene.Id","Domain.Name","Start.DomPos","End.DomPos","Domain.Length")
@@ -20,6 +19,6 @@ uniq.Domains<- length(unique(hmmrHit[,2]))
 #                     find the mutation position in each isolate                      #
 #                                                                                    #
 #------------------------------------------------------------------------------------#
-inputs<- list.files("/intersection/")
+inputs<- list.files("/intersections/")
 reference_Genome<- as.matrix(read.table("/nctc8325.bed",header=F,sep="\t",stringsAsFactors = F))
 MutationPosition(reference_Genome,inputs)
