c56fd2b0007a259585b65df07ccafd6b68d5d5bf
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2019-10-29 12:46:05 -0500

    Update pythonvarpos.py

--- pythonvarpos.py
+++ pythonvarpos.py
@@ -11,12 +11,9 @@ outputPath = sys.argv[3]
 
 outputFile = "varpos.sh"
 with open(outputFile,'w') as outFile:
-    outFile.write(f'sed -i \'s/^cpdna/chr/\' {genomeBedpath}nctc8325.bed;\n')  
-    count=0
     with open(inputFile) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         for row in csv_reader:
-            if count !=0:                          
                 outFile.write(f'reference_Genome <- as.matrix(read.table(paste0({GenomebedPath},"/nctc8325.bed"),header=F,sep="\t",stringsAsFactors = F))\n')
                 outFile.write('length.genome<- nrow(reference_Genome) \n')
                 outFile.write(f'intersections <- read.table(paste({intersectionspath},{row[0]}.bed,sep=""),header=F,sep="\t",stringsAsFactors = F)\n')
@@ -26,7 +23,7 @@ with open(outputFile,'w') as outFile:
                 outFile.write('for (k in 1:length.intersection) \n')
                 outFile.write('for (j in 1 :length.genome)\n')
                   outFile.write('if (intersections[k,2] >= reference_Genome[j,2] & intersections[k,2] <= reference_Genome[j,3]) \n')
-                   outFile.write(' { \n')
+                  outFile.write(' { \n')
                      outFile.write('variant.matrix[k,1] = as.character(reference_Genome [j,4])\n')
                      outFile.write('z<-round(abs(as.numeric(intersections[k,2]) - as.numeric(reference_Genome [j,2]))/3)\n')
                      outFile.write('variant.matrix[k,2] = z\n')
@@ -35,11 +32,7 @@ with open(outputFile,'w') as outFile:
                      outFile.write('variant.matrix[k,4] = m\n')
                      outFile.write('variant.matrix[k,5] = round(m/3)\n')
                     outFile.write(' }\n')
-        outFile.write('variant.matrix<- variant.matrix[complete.cases(variant.matrix),]\n')')
-        outFile.write( f'write.csv(x=variant.matrix,file = paste(paste0({outputPath},"/VariantPosition/"),paste0(row[0],".csv"),sep=""), row.names = FALSE)\n')
+               outFile.write('variant.matrix<- variant.matrix[complete.cases(variant.matrix),]\n')')
+               outFile.write( f'write.csv(x=variant.matrix,file = paste(paste0({outputPath},"/VariantPosition/"),paste0(row[0],".csv"),sep=""), row.names = FALSE)\n')
 
                 
-
-                
-
-            count =count + 1
