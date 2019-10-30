56e6a5a59fb2b5fdbd7d73800bf2dd509e6c0c2d
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2019-10-30 11:49:37 -0500

    Update mappinghmmerhit.R

--- mappinghmmerhit.R
+++ mappinghmmerhit.R
@@ -8,18 +8,18 @@ bigtableWeightFile<-argv[6]
 #---------------------------------------------------------------------------------------------------#
 #                 Please Change the Setwd to the location you download the folder                   #
 #---------------------------------------------------------------------------------------------------#
-#source("geneDomain.R")
+source("geneDomain.R")
 source(paste0(sourcePath,"/MutationPosition.R"))
 #-----------------------------------------------------------------------#
 #                             read hmmrfiles                             #
 #-----------------------------------------------------------------------#
-#hmmrHit <- as.matrix(read.table(paste0(outputPath,"hmmrfile/sort.hit.S.areus.csv"),header=F,sep=" ",stringsAsFactors = F))
-#length.hmmrHit <-  nrow(hmmrHit)
+hmmrHit <- read.table(paste0(outputPath,"hmmrfile/sort.hit.S.areus.csv"),header=F,sep=" ",stringsAsFactors = F)
+length.hmmrHit <-  nrow(hmmrHit)
 intersections<- list.files(intersectionspath)
-#Domain.length<- as.numeric(hmmrHit[,4]) - as.numeric(hmmrHit[,3])
-#hmmrHit<-cbind(hmmrHit,Domain.length)
-#colnames(hmmrHit)<- c("Gene.Id","Domain.Name","Start.DomPos","End.DomPos","Domain.Length")
-#uniq.Domains<- length(unique(hmmrHit[,2]))
+Domain.length<- as.numeric(hmmrHit[,4]) - as.numeric(hmmrHit[,3])
+hmmrHit<-cbind(hmmrHit,Domain.length)
+colnames(hmmrHit)<- c("Gene.Id","Domain.Name","Start.DomPos","End.DomPos","Domain.Length")
+uniq.Domains<- length(unique(hmmrHit[,2]))
 
 #------------------------------------------------------------------------------------#
 #                                                                                    #
@@ -28,3 +28,18 @@ intersections<- list.files(intersectionspath)
 #------------------------------------------------------------------------------------#
 reference_Genome<- read.table(paste0(GenomebedPath,"/nctc8325.bed"),header=F,sep="\t",stringsAsFactors = F)
 MutationPosition(reference_Genome,intersections,intersectionspath,outputPath)
+#------------------------------------------------------------------------------------#
+#                                                                                    #
+#                     find the mutaion domain   in each isolate                      #
+#                                                                                    #
+#------------------------------------------------------------------------------------#
+Domain.Isolate <- matrix(0,nrow=length(intersections),ncol=uniq.Domains)
+isolates<-gsub(pattern = ".bed",replacement = "",intersections, perl = T)
+rownames(Domain.Isolate) <- isolates
+colnames(Domain.Isolate) <- unique(hmmrHit[,2])
+Domain.Isolate.norm<-Domain.Isolate
+for (i in 1:length(intersections))
+{
+  domain.per.isolate <- geneDomain.vcf(intersections[i],hmmrHit)
+  Domain.Isolate[i,names(domain.per.isolate)] <-  as.numeric(domain.per.isolate) 
+}
