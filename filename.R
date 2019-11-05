c9f8ebbf3feb2a9205ba47db841718fe9ea7ccc2
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2019-11-05 14:33:01 -0600

    Update filename.R

--- filename.R
+++ filename.R
@@ -2,4 +2,4 @@ argv <- commandArgs(trailingOnly = TRUE)
 InputPath <- argv[1]
 OutputFile <- argv[2]
 filename <- list.files(InputPath )
-write.csv(filename , OutputFile, row.names = F, quote=FALSE)
+write.table(filename , OutputFile,  col.names = F, row.names = F , quote=FALSE)
